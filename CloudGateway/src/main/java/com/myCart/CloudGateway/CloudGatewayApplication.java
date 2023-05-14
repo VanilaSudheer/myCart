43159e5a15b733aca552940d4ca4377b781a6c85
sudhe <vanisudheer@gmail.com>
2023-05-14 16:02:28 -0400

    circuit breaker

--- CloudGateway/src/main/java/com/myCart/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/myCart/CloudGateway/CloudGatewayApplication.java
@@ -9,5 +9,14 @@ public class CloudGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(
+				id -> new Resilience4JConfigBuilder(id)
+						.circuitBreakerConfig(
+								CircuitBreakerConfig.ofDefaults()
 
+						).build()
+		);
+	}
 }
